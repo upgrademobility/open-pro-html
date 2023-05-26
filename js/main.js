b5d1221c9a9f0d8847934f277265786adbc28f38
Daniel Bogdoll <bogdoll@fzi.de>
2023-05-26 16:18:25 +0200

    Working quite well

--- js/main.js
+++ js/main.js
@@ -5,3 +5,19 @@ AOS.init({
   easing: 'ease-out-sine',
 });
 
+const carouselEl = document.querySelectorAll('.carousel');
+if (carouselEl.length > 0) {
+  const carousel = new Swiper('.carousel', {
+    slidesPerView: 'auto',
+    grabCursor: true,
+    loop: false,
+    centeredSlides: false,
+    initialSlide: 0,
+    spaceBetween: 24,
+    watchSlidesProgress: true,
+    navigation: {
+      nextEl: '.carousel-next',
+      prevEl: '.carousel-prev',
+    },
+  });
+}
